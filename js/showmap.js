66c8edefd74dbeeec63ca293cb888df45fe86df9
Konstantinos76 <konstantinos_@outlook.com>
2018-07-14 02:41:04 +0300

    Stage-2

---
+++ js/showmap.js
@@ -0,0 +1,4 @@
+document.getElementById('mapButton').addEventListener('click', function(){
+    document.getElementById('map').style.display = 'block';
+    document.getElementById('mapButton').style.display = 'none';
+  })
--- service-worker.js
+++ service-worker.js
@@ -8,19 +8,19 @@ self.addEventListener('install', function(event) {
         'js/index.js',
         'js/main.js',
         'js/restaurant_info.js',
-        'img/1.jpg',
-        'img/2.jpg',
-        'img/3.jpg',
-        'img/4.jpg',
-        'img/5.jpg',
-        'img/6.jpg',
-        'img/7.jpg',
-        'img/8.jpg',
-        'img/9.jpg',
-        'img/10.jpg'
+        'img/1.webp',
+        'img/2.webp',
+        'img/3.webp',
+        'img/4.webp',
+        'img/5.webp',
+        'img/6.webp',
+        'img/7.webp',
+        'img/8.webp',
+        'img/9.webp',
+        'img/10.webp'
     ]
     event.waitUntil(
-        caches.open('cache-v1.5').then(function(cache) {
+        caches.open('cache-v2').then(function(cache) {
             return cache.addAll(urlsToCache);
         })
     );
